da6a31699e0d501f4f162ede46ab9cc657dbb8b5
Samantha Wu <samanthayywu@gmail.com>
2022-03-26 17:43:29 +0000

    bits & memory

--- main.go
+++ main.go
@@ -4,5 +4,17 @@ import "fmt"
 
 // only one main func at the entry file. must called main to match with package
 func main() {
-	fmt.Println("hey, ninjas")
+	var nameOne string = "mario"
+	nameTwo := "luigi"
+	// both are same. := shorthand cannot be used out
+
+	fmt.Println(nameOne, nameTwo)
+
+	// bits & memory
+
+	var numOne int = 89
+	var numTwo uint = 23
+	// uint are only positive numbers
+	var numThree int8 = -12
+	// int8 ranges from -128 to 127
 }
