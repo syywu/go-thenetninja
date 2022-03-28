e14a3dbab5cfae5463b851c57ee19d92f477fe01
Samantha Wu <samanthayywu@gmail.com>
2022-03-28 21:31:45 +0100

    updated struct & custom types

--- bill.go
+++ bill.go
@@ -5,3 +5,16 @@ type bill struct {
 	items map[string]float64
 	tip   float64
 }
+
+// make new bills
+func newBill(name string) bill {
+	b := bill{
+		name:  name,
+		items: map[string]float64{}, //makes an empty map
+		tip:   0,                    //sets tip to 0
+	}
+	return b
+
+	// b is a bill object of inital values
+
+}
--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+import "fmt"
+
 func main() {
+
+	mybill := newBill("mario's bill")
+
+	fmt.Println(mybill) //returns {mario's bill map[] 0}
 
 }
