91487b0881649302fc9cf51846ba150caccd4775
Samantha Wu <samanthayywu@gmail.com>
2022-03-29 17:17:31 +0100

    get input func

--- main.go
+++ main.go
@@ -4,7 +4,16 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
+
+// helper func so dont have to type out the same code everytime we want the user to type in something. just call this
+func getInput(prompt string, r *bufio.Reader) (string, error) {
+	fmt.Print(prompt)
+	input, err := r.ReadString('\n')
+
+	return strings.TrimSpace(input), err
+}
 
 //create user input instead of hardcoding codes
 func creatBill() bill {
@@ -12,11 +21,29 @@ func creatBill() bill {
 	reader := bufio.NewReader(os.Stdin)
 	// the standard input is the terminal. new reader reads info from terminal
 
+	// fmt.Print("Create a new bill name: ")
+	// name, _ := reader.ReadString('\n') //read str after the new line char is enetered
+	// // \n is essentially the enter key. reader will read after user has typed in enter. whta they entered will be captured in the name var
+
+	// // get rid of whitespace entered
+	// name = strings.TrimSpace(name)
+
+	name, _ := getInput("Create a new bill name: ", reader)
+	// this is much easier in the future than type in all of the above. makes it much more reusable
+
+	// storing whatever typed to b var
+	b := newBill(name)
+	fmt.Print("Created new bill - ", b.name, "\n")
+
+	return b
+
 }
 
 func main() {
 
 	mybill := creatBill()
+
+	fmt.Println(mybill) //{sam's  bill map[] 0}
 
 	// mybill := newBill("mario's bill")
 
@@ -39,6 +66,8 @@ func main() {
 
 	//fmt.Println(mybill) //returns {mario's bill map[] 0}
 
-	fmt.Println(mybill.format()) //pie: ... $5.99 cake: ... $3.99 total:                    ...$9.98
+	// fmt.Println(mybill.format()) //pie: ... $5.99 cake: ... $3.99 total:                    ...$9.98
 
 }
+
+// option for user to add items or tips, or even to save the bill
