3a07d509b399ed6eb3a21531f391f82296432e6d
Samantha Wu <samanthayywu@gmail.com>
2022-03-29 16:19:00 +0100

    reader and stadin

--- main.go
+++ main.go
@@ -1,17 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+//create user input instead of hardcoding codes
+func creatBill() bill {
+	// get user input info by using a reader to read info that a user types in
+	reader := bufio.NewReader(os.Stdin)
+	// the standard input is the terminal. new reader reads info from terminal
+
+}
 
 func main() {
 
-	mybill := newBill("mario's bill")
+	mybill := creatBill()
 
-	mybill.addItems("onion soup", 4.50)
-	mybill.addItems("veg pie", 8.95)
-	mybill.addItems("toffee pudding", 4.95)
-	mybill.addItems("coffee", 3.25)
+	// mybill := newBill("mario's bill")
 
-	mybill.updatedTip(10)
+	// mybill.addItems("onion soup", 4.50)
+	// mybill.addItems("veg pie", 8.95)
+	// mybill.addItems("toffee pudding", 4.95)
+	// mybill.addItems("coffee", 3.25)
+
+	// mybill.updatedTip(10)
+	/*
+		Bill Breakdown:
+		onion soup:               ...$4.5
+		veg pie:                  ...$8.95
+		toffee pudding:           ...$4.95
+		coffee:                   ...$3.25
+		tip:                      ...$10.00
+		total:                    ...$31.65
+
+	*/
 
 	//fmt.Println(mybill) //returns {mario's bill map[] 0}
 
