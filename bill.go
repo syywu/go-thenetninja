8d144b5be2aebd9eefc94f784d0af943f35abb3a
Samantha Wu <samanthayywu@gmail.com>
2022-03-29 14:08:56 +0100

    updated receiver func

--- bill.go
+++ bill.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type bill struct {
 	name  string
 	items map[string]float64
@@ -10,8 +12,8 @@ type bill struct {
 func newBill(name string) bill {
 	b := bill{
 		name:  name,
-		items: map[string]float64{}, //makes an empty map
-		tip:   0,                    //sets tip to 0
+		items: map[string]float64{"pie": 5.99, "cake": 3.99}, //makes an empty map
+		tip:   0,                                             //sets tip to 0
 	}
 	return b
 }
@@ -21,4 +23,27 @@ func newBill(name string) bill {
 // format the bill- receiver function. limits the function only to bill object. (b bill) b of type bill
 func (b bill) format() string {
 
+	fs := "Bill Breakdown: \n" //fs = formated strings
+	var total float64 = 0
+
+	//list items
+	for k, v := range b.items {
+		fs += fmt.Sprintf("%-25v ...$%v \n", k+":", v) //adding items to fs
+		total += v                                     //adding the value to total
+	}
+
+	// add total
+	fs += fmt.Sprintf("%-25v ...$%0.2f", "total:", total) //-25 makes the val 25 characters long which makes the list more inline
+	/* pie:                      ...$5.99
+	   cake:                     ...$3.99
+	   total:                    ...$9.98
+
+	   adding : individually from %v, otherwise will turn out like:
+
+	   pie                     : ...$5.99
+
+
+	*/
+
+	return fs
 }
--- main.go
+++ main.go
@@ -8,4 +8,6 @@ func main() {
 
 	fmt.Println(mybill) //returns {mario's bill map[] 0}
 
+	fmt.Println(mybill.format()) //pie: ... $5.99 cake: ... $3.99 total:                    ...$9.98
+
 }
